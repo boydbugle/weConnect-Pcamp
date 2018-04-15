9a7346d1b6de7beca8395a6d3028c39498b237c4
boyd ndonga <boydbugle@gmail.com>
2018-04-15 23:09:23 +0300

    update requirements.txt

---
+++ app/__init__.py
@@ -0,0 +1,4 @@
+"""This constructor imports most of the Flask extensions currently in use. The create_app() function is the
+   application factory, which takes as an argument the name of a configuration to use for the
+   application."""
+
