fcd4f172127eba356d2b688a4f951f082a25f19f
boyd ndonga <boydbugle@gmail.com>
2018-04-22 19:29:56 +0300

    refactor path on registration test suite

--- tests/test_user.py
+++ tests/test_user.py
@@ -27,40 +27,40 @@ class UserTestCase(unittest.TestCase):
             data=json.dumps(user_data)
         )
 
-    def test_user_registration(self, email="app@test.com", password="app1541test"):
+    def test_user_registration(self, email="app@test.com", password="app1541test", path='/api/v1/register'):
         # test successful registration
-        res = self.get_agent_request(email, password, '/api/v1/register')
+        res = self.get_agent_request(email, password, path)
         result = json.loads(res.data.decode())
         self.assertEqual(result['message'], 'successfully created user')
         self.assertEqual(res.status_code, 201)
 
         # test valid email
-        res = self.get_agent_request('apptest', password, '/api/v1/register')
+        res = self.get_agent_request('apptest', password, path)
         result = json.loads(res.data.decode())
         self.assertEqual(result['message'], 'invalid email')
         self.assertEqual(res.status_code, 403)
 
         # test blank email
-        res = self.get_agent_request('', password, '/api/v1/register')
+        res = self.get_agent_request('', password, path)
         result = json.loads(res.data.decode())
         self.assertEqual(result['message'], 'invalid email')
         self.assertEqual(res.status_code, 403)
 
         # test valid password
-        res = self.get_agent_request(email, password, '/api/v1/register')
+        res = self.get_agent_request(email, password, path)
         result = json.loads(res.data.decode())
         self.assertEqual(result['message'], 'successfully created user')
         self.assertEqual(res.status_code, 201)
 
         # test invalid password
-        res = self.get_agent_request(email, '48d@j', '/api/v1/register')
+        res = self.get_agent_request(email, '48d@j', path)
         result = json.loads(res.data.decode())
         self.assertEqual(result['message'], 'invalid password')
         self.assertEqual(result['hint'], 'password atleast 8 characters of numbers/letters/special char')
         self.assertEqual(res.status_code, 403)
 
         # test blank password
-        res = self.get_agent_request(email, '', '/api/v1/register')
+        res = self.get_agent_request(email, '', path)
         result = json.loads(res.data.decode())
         self.assertEqual(result['message'], 'invalid password')
         self.assertEqual(res.status_code, 403)
@@ -68,7 +68,7 @@ class UserTestCase(unittest.TestCase):
         # test registration has no get request
         user_data = {'email': email, 'password': password}
         res = self.client().get(
-            '/api/v1/register',
+            path,
             headers=self.get_api_headers(),
             data=json.dumps(user_data)
         )
@@ -78,8 +78,8 @@ class UserTestCase(unittest.TestCase):
         self.assertEqual(res.status_code, 404)
 
     # def test_duplicate_registration(self, email="app@test.com", password="app1541test"):
-    #     res = self.get_agent_request(email, password, '/api/v1/register')
-    #     res2 = self.get_agent_request(email, password, '/api/v1/register')
+    #     res = self.get_agent_request(email, password, path)
+    #     res2 = self.get_agent_request(email, password, path)
     #     result = json.loads(res2.data.decode())
     #     self.assertEqual(result['error'], 'user in existence')
     #     self.assertEqual(res2.status_code, 406)
