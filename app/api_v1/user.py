0f287c93954600cf1caa61e6a793d79981bddac4
boyd ndonga <boydbugle@gmail.com>
2018-04-24 01:21:06 +0300

    update requirements.txt to reflect current state

--- app/api_v1/user.py
+++ app/api_v1/user.py
@@ -6,9 +6,11 @@ class User:
         self.email = email
         self.pw_hash = password
         # self.user_id = user_id
+
     def set_password(self, password):
         self.pw_hash = generate_password_hash(password)
         # print(self.pw_hash)
+        
     def check_password(self, password):
         return check_password_hash(self.pw_hash, password)
 
