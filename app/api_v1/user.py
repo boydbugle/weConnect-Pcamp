bf753d5eec3acee04e75b86d5010b9dc7d88c1b7
boyd ndonga <boydbugle@gmail.com>
2018-04-30 08:00:54 +0300

    generate json web token

--- app/api_v1/user.py
+++ app/api_v1/user.py
@@ -1,4 +1,6 @@
 from werkzeug.security import generate_password_hash
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer, BadData, BadSignature
+from flask import current_app
 
 
 class User:
@@ -8,6 +10,18 @@ class User:
 
     def set_password(self, password):
         self.pw_hash = generate_password_hash(password)
+
+    @staticmethod
+    def generate_token(email):
+        try:
+            s = Serializer(current_app.config['SECRET_KEY'], expires_in=1800)
+            return s.dumps({'email': email})
+        except Exception as e:
+            return str(e)
+
+    @staticmethod
+    def decode_token(token):
+        s = Serializer(current_app.config['SECRET_KEY'])
 
     def __str__(self):
         user = {
